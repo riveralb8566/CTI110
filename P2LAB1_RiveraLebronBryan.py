56acca072a576b26957c508c2471ba453ee0e14e
riveralb8566 <99390828+riveralb8566@users.noreply.github.com>
2022-02-21 22:49:20 -0500

    P2LAB1
    P2LAB1

---
+++ P2LAB1_RiveraLebronBryan.py
@@ -0,0 +1,12 @@
+user_int = int(input('Enter integer (32 - 126):\n'))
+
+
+float = float(input('Enter float:\n'))
+character = str(input('Enter character:\n'))
+string = str(input('Enter string:\n'))
+
+print (user_int, float, character, string) 
+
+print (string, character, float, user_int)
+
+print (user_int, "converted to a character is", chr(user_int))
