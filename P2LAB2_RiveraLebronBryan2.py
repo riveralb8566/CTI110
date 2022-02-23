6f39a6117b8d7dc4bad57233dc09fc78c609f91a
riveralb8566 <99390828+riveralb8566@users.noreply.github.com>
2022-02-22 23:03:02 -0500

    P2LAB2_RiveraLebronBryan2.
    P2LAB2_RiveraLebronBryan2

---
+++ P2LAB2_RiveraLebronBryan2.py
@@ -0,0 +1,9 @@
+current_price = int(input())
+last_months_price = int(input())
+
+''' Type your code here. '''
+
+change = current_price - last_months_price
+new_montage = (current_price * 0.051) / 12
+
+print("This house is $", format(current_price,'.0f'),".", " The change is $", format(change,'.0f')," since last month.", "\nThe estimated monthly mortgage is $", format(new_montage,'.2f'),".",sep='')
